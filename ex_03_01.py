a437ead8c07bbd31e69fb92df7338e9ad5566145
gustavo-dotcom <gustavo.oliveira2003@hotmail.com>
2023-09-17 22:49:43 -0300

    mudanças para o codigo multiplicar 1.5 na taxa a ser paga quando as horas forem maiores que 40

--- ex_03_01.py
+++ ex_03_01.py
@@ -1,4 +1,9 @@
-hours = float(input("Enter hors: "))    #user input hours and convert to float
-rate = float(input("Enter rate: "))     #user input rate per hour
-pay = hours * rate                      #multiply hours and rate to create the pay variable
-print("Pay:", pay)                      #print pay variable
+hours = float(input("Enter hors: "))        #user input hours and convert to float
+rate = float(input("Enter rate: "))         #user input rate per hour
+regular_pay = hours * rate                  #multiply hours and rate to create the pay variable
+overtime_pay = (hours - 40) * (rate * 0.5)  #calculate overtime to pay 1.5 times above 40 hours
+
+if hours > 40:
+    print("Overtime, pay:", regular_pay + overtime_pay)
+else:
+    print("Regular, pay: ", regular_pay)
