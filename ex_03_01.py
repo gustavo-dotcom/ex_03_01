6ec4e136c5823e811ff4e34c49378d46e8690c21
gustavo-dotcom <gustavo.oliveira2003@hotmail.com>
2023-09-17 22:25:24 -0300

    código multiplicando horas vezes a taxa para gerar um valor a ser pago

---
+++ ex_03_01.py
@@ -0,0 +1,4 @@
+hours = float(input("Enter hors: "))    #user input hours and convert to float
+rate = float(input("Enter rate: "))     #user input rate per hour
+pay = hours * rate                      #multiply hours and rate to create the pay variable
+print("Pay:", pay)                      #print pay variable
